adef3c10c2b66c484065153ad25070d91681cb2c
Pranjal Pruthi <47497714+pranjalpruthi@users.noreply.github.com>
2024-05-01 16:33:37 +0530

    new

--- bbfetch.sh
+++ bbfetch.sh
@@ -7,28 +7,44 @@ echo "Please enter the number of parallel jobs you want to run:"
 read num_jobs
 
 max_attempts=10
+log_dir="./log"
+log_file="$log_dir/failed_accessions_$(date +%Y%m%d_%H%M%S).txt"
+
+# Create log directory if it doesn't exist
+mkdir -p "$log_dir"
 
 download() {
  accession=$1
  declare -i attempt=0
  while [[ $attempt -le $max_attempts ]]; do
     echo "Attempt $(($attempt + 1)) of $max_attempts for accession $accession"
-    datasets download genome accession "$accession" --filename "$accession.zip"
-    if unzip -t "$accession.zip" &>/dev/null; then
-      echo "Download and integrity check successful for accession $accession."
-      return 0
-    else
-      echo "Download failed or file is corrupted for accession $accession, retrying..."
-      ((attempt++))
-      if [[ $attempt -eq $max_attempts ]]; then
-        echo "Maximum attempts reached, download failed for accession $accession."
-        return 1
+    if datasets download genome accession "$accession" --filename "$accession.zip"; then
+      if unzip -t "$accession.zip" &>/dev/null; then
+        echo "Download and integrity check successful for accession $accession."
+        return 0
+      else
+        echo "Download failed or file is corrupted for accession $accession, retrying..."
+        # Log the failed accession to the log file
+        echo "$accession" >> "$log_file"
       fi
+    else
+      echo "Error occurred during download for accession $accession, retrying..."
+      # Log the failed accession to the log file
+      echo "$accession" >> "$log_file"
+    fi
+    ((attempt++))
+    if [[ $attempt -eq $max_attempts ]]; then
+      echo "Maximum attempts reached, download failed for accession $accession."
+      # Log the failed accession to the log file
+      echo "$accession" >> "$log_file"
+      return 1
     fi
  done
 }
 
+
 export -f download
+export log_file
 
 # Use GNU Parallel to run downloads in parallel with progress bar
 parallel --will-cite --progress -a "$filename" -j "$num_jobs" download
